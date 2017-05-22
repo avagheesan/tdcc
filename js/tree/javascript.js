bfaef51a9171e30e88fc6a3e5b1959d53b2ad7fa
avagheesan <arunprakash.vagheesan@epsilon.com>
2017-05-22 01:45:43 -0500

    Add files via upload

---
+++ js/tree/javascript.js
@@ -0,0 +1,79 @@
+$(function () {
+    var
+        $table = $('#tree-table'),
+        rows = $table.find('tr');
+
+    rows.each(function (index, row) {
+        var
+            $row = $(row),
+            level = $row.data('level'),
+            id = $row.data('id'),
+            $columnName = $row.find('td[data-column="name"]'),
+            children = $table.find('tr[data-parent="' + id + '"]');
+
+        if (children.length) {
+            var expander = $columnName.prepend('' +
+                '<span class="treegrid-expander glyphicon glyphicon-chevron-right"></span>' +
+                '');
+
+            children.hide();
+
+            expander.on('click', function (e) {
+                var $target = $(e.target);
+                if ($target.hasClass('glyphicon-chevron-right')) {
+                    $target
+                        .removeClass('glyphicon-chevron-right')
+                        .addClass('glyphicon-chevron-down');
+
+                    children.show();
+                } else {
+                    $target
+                        .removeClass('glyphicon-chevron-down')
+                        .addClass('glyphicon-chevron-right');
+
+                    reverseHide($table, $row);
+                }
+            });
+        }
+
+        $columnName.prepend('' +
+            '<span class="treegrid-indent" style="width:' + 15 * level + 'px"></span>' +
+            '');
+    });
+
+    // Reverse hide all elements
+    reverseHide = function (table, element) {
+        var
+            $element = $(element),
+            id = $element.data('id'),
+            children = table.find('tr[data-parent="' + id + '"]');
+
+        if (children.length) {
+            children.each(function (i, e) {
+                reverseHide(table, e);
+            });
+
+            $element
+                .find('.glyphicon-chevron-down')
+                .removeClass('glyphicon-chevron-down')
+                .addClass('glyphicon-chevron-right');
+
+            children.hide();
+        }
+    };
+});
+
+$(function () {
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    $('.tree li.parent_li > span').on('click', function (e) {
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(":visible")) {
+            children.hide('fast');
+            $(this).attr('title', 'Expand this branch').find(' > i').addClass('icon-plus-sign').removeClass('icon-minus-sign');
+        } else {
+            children.show('fast');
+            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('icon-minus-sign').removeClass('icon-plus-sign');
+        }
+        e.stopPropagation();
+    });
+});
